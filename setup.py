e2ef29549a594081cbef2d238fb1efdec18b519e
Josh Briegal <josh@briegal.com>
2020-02-21 11:45:47 +0000

    Add Manifest.in (#10)
    * add manifest.in
    
    * move pybind11
    
    * add submodule in cmakelists
    
    * simplify requrements and setup
    
    * delete submodules
    
    * alter workflow

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ from distutils.version import LooseVersion
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
+    def __init__(self, name, sourcedir="", *args, **kwargs):
+        Extension.__init__(self, name, sources=[], *args, **kwargs)
         self.sourcedir = os.path.abspath(sourcedir)
 
 
 class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
@@ -26,6 +26,7 @@ class PyTest(TestCommand):
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
@@ -33,14 +34,18 @@ class PyTest(TestCommand):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            out = subprocess.check_output(["cmake", "--version"])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
 
         if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
+            cmake_version = LooseVersion(
+                re.search(r"version\s*([\d.]+)", out.decode()).group(1)
+            )
+            if cmake_version < "3.1.0":
                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
         for ext in self.extensions:
@@ -48,48 +53,70 @@ class CMakeBuild(build_ext):
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+        ]
 
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
+            cmake_args += [
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+            ]
+            if sys.maxsize > 2 ** 32:
+                cmake_args += ["-A", "x64"]
+            build_args += ["--", "/m"]
         else:
             try:
-                GPP_COMPILER = subprocess.check_output("which g++", shell=True).decode().strip('\n\t\r')
-                GCC_COMPILER = subprocess.check_output("which gcc", shell=True).decode().strip('\n\t\r')
+                GPP_COMPILER = (
+                    subprocess.check_output("which g++", shell=True)
+                    .decode()
+                    .strip("\n\t\r")
+                )
+                GCC_COMPILER = (
+                    subprocess.check_output("which gcc", shell=True)
+                    .decode()
+                    .strip("\n\t\r")
+                )
             except subprocess.CalledProcessError as e:
                 GPP_COMPILER, GCC_COMPILER = None, None
             else:
-                cmake_args += ['-DCMAKE_CXX_COMPILER=' + GPP_COMPILER,
-                               '-DCMAKE_C_COMPILER=' + GCC_COMPILER]
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
+                cmake_args += [
+                    "-DCMAKE_CXX_COMPILER=" + GPP_COMPILER,
+                    "-DCMAKE_C_COMPILER=" + GCC_COMPILER,
+                ]
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+            build_args += ["--", "-j2"]
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get("CXXFLAGS", ""), self.distribution.get_version()
+        )
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
+
 
 setup(
-    name='gacf',
-    version='0.3',
-    author='Joshua Briegal',
-    author_email='jtb34@cam.ac.uk',
-    description='A generalisation of the autocorrelation function, for non-uniformly sampled timeseries data',
-    long_description='',
-    packages=['gacf'],
-    ext_modules=[CMakeExtension('gacf.correlator', 'gacf'), CMakeExtension('gacf.datastructure', 'gacf')],
-    cmdclass=dict(build_ext=CMakeBuild,
-                  test=PyTest),
-    tests_require=['pytest'],
-    zip_safe=False
+    name="gacf",
+    version="0.3",
+    author="Joshua Briegal",
+    author_email="jtb34@cam.ac.uk",
+    description="A generalisation of the autocorrelation function, for non-uniformly sampled timeseries data",
+    long_description="",
+    packages=["gacf"],
+    ext_modules=[
+        CMakeExtension("gacf.correlator", "gacf"),
+        CMakeExtension("gacf.datastructure", "gacf"),
+    ],
+    cmdclass=dict(build_ext=CMakeBuild, test=PyTest),
+    tests_require=["pytest"],
+    zip_safe=False,
 )
